53c9251c3fde63d1d92a9c47c6f3f0175d7f3b98
Devon Bahary <devon.bahary@robinhealthcare.com>
2020-12-14 09:35:34 -0800

    adds static folder serving for production

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import express from "express";
+import path from "path";
 import bodyParser from "body-parser";
 import "./database/mongo-client";
 import notes from "./routes/notes";
@@ -8,5 +9,14 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use('/notes', notes);
+
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static(path.resolve(__dirname, 'client')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+    });
+}
 
 app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`));
